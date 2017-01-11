2575582b17f6f0001bd5e3be7c4373e498c2eaf1
Raul Kist <raulkist@gmail.com>
2017-01-11 23:01:23 +0100

    wee7.1

--- 7/machine-learning-ex6/ex6/gaussianKernel.m
+++ 7/machine-learning-ex6/ex6/gaussianKernel.m
@@ -7,7 +7,7 @@ function sim = gaussianKernel(x1, x2, sigma)
 x1 = x1(:); x2 = x2(:);
 
 % You need to return the following variables correctly.
-sim = 0;
+%sim = 0;
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Fill in this function to return the similarity between x1
@@ -16,8 +16,11 @@ sim = 0;
 %
 %
 
+deltaXSquare = sum((x1-x2).^2);
 
+expTerm = -deltaXSquare / (2*sigma^2);
 
+sim = exp(expTerm);
 
 
 
